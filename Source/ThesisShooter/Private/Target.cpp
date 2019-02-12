da08dd5c57d1622129dd9f6f99d52c595f323e3a
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2019-02-12 14:12:44 +0100

    Target and area scoring

---
+++ Source/ThesisShooter/Private/Target.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "../Public/Target.h"
+
+// Sets default values
+ATarget::ATarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATarget::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATarget::NotifyHit(class UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Recieve Hit!"));
+	if (!Centre) { return; }
+	float DistanceFromCentre = (HitLocation - Centre->GetComponentLocation()).Size();
+	if (DistanceFromCentre > TargetRadius) { return; }
+	else if (DistanceFromCentre > MiddleRingRadius)
+	{
+		OnTargetHitDelegate.Broadcast(OuterRingScore);
+	}
+	else if (DistanceFromCentre > InnerCircleRadius)
+	{
+		OnTargetHitDelegate.Broadcast(MiddleRingScore);
+	}
+	else
+	{
+		OnTargetHitDelegate.Broadcast(InnerCircleScore);
+	}
+
+}
+
+void ATarget::FindCentreComponent(USceneComponent* CentreComponent)
+{
+	Centre = CentreComponent;
+}
+
---
+++ Source/ThesisShooter/Public/Target.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Target.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTargetDelegate, int, score);
+
+UCLASS()
+class THESISSHOOTER_API ATarget : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATarget();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float TargetRadius = 50;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float MiddleRingRadius = 40;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float InnerCircleRadius = 20;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	int OuterRingScore = 1;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	int MiddleRingScore = 2;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	int InnerCircleScore = 4;
+
+	FTargetDelegate OnTargetHitDelegate;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	USceneComponent* Centre;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+
+	UFUNCTION(BlueprintCallable)
+	void FindCentreComponent(USceneComponent* CentreComponent);
+
+	virtual void NotifyHit(class UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+};
