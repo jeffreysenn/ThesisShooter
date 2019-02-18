f83ef2dcae5cddd0f2efca35e50bb1b7a997c8db
M1m1c <ludvigbaumolss@hotmail.se>
2019-02-18 16:54:55 +0100

    Changed text formating
    Made it so that we can print out our data variables

--- Source/ThesisShooter/Private/ThesisSHooterPlayerController.cpp
+++ Source/ThesisShooter/Private/ThesisSHooterPlayerController.cpp
@@ -49,9 +49,19 @@ void AThesisSHooterPlayerController::SaveDataToFile()
 {
 
 	UE_LOG(LogTemp, Warning, TEXT("@SaveDataToFile saving data to file "));
+	FString DataContent = FString("Participant: " + PlayerIndex + LINE_TERMINATOR
+		+ "Shots that hit target: " + FString::FromInt(ShotHits) + LINE_TERMINATOR
+		+ "Total Shots fired: " + FString::FromInt(Shots) + LINE_TERMINATOR
+		//+ "Amount of Targets hit: " +  FString::FromInt(TargetsHit) + LINE_TERMINATOR
+		+ "Accuracy score: " + FString::FromInt(AccuracyScore) + LINE_TERMINATOR
+		+ LINE_TERMINATOR
+		+ "First section time: " + FString::SanitizeFloat(FirstSectionTime) + LINE_TERMINATOR
+		+ "Second section time: "+ FString::SanitizeFloat(SecondSectionTime) + LINE_TERMINATOR
+		+ "Third section time: " + FString::SanitizeFloat(ThirdSectionTime) + LINE_TERMINATOR
+		+"Total time: " + FString::SanitizeFloat(FirstSectionTime + SecondSectionTime + ThirdSectionTime) + LINE_TERMINATOR + LINE_TERMINATOR + LINE_TERMINATOR + LINE_TERMINATOR);
 
 	FString FilePath = FPaths::ConvertRelativePathToFull(FPaths::GameSavedDir()) + TEXT("/DataLog"+ PlayerIndex +".txt");
-	FString FileContent = TEXT("This is a line of text to put in the file.\n");
+	FString FileContent = TEXT(""+DataContent);
 	FFileHelper::SaveStringToFile(FileContent, *FilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), EFileWrite::FILEWRITE_Append);
 
 }
